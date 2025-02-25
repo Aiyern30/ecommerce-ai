a4e0618c435ec2040148a337fea85cc7fd35d7cd
Aiyern30 <ianbian2@gmail.com>
2025-02-25 15:40:47 +0800

    Create extra layout for header and footer and put under root layout

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+import type React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
