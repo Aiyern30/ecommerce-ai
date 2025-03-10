987e60b5081b396f57e4b2051f714411ec267cec
Aiyern30 <ianbian2@gmail.com>
2025-03-10 11:32:24 +0800

    `Refactor Notifications page layout and add link to view notifications`

--- app/Notifications/page.tsx
+++ app/Notifications/page.tsx
@@ -6,27 +6,28 @@ export default function NotificationsPage() {
   return (
     <div className="container mx-auto mb-4">
       <NotificationsHeader />
-
-      <Tabs defaultValue="all" className="mt-6">
-        <TabsList className="grid w-full grid-cols-4 md:w-auto">
-          <TabsTrigger value="all">All</TabsTrigger>
-          <TabsTrigger value="orders">Orders</TabsTrigger>
-          <TabsTrigger value="promotions">Promotions</TabsTrigger>
-          <TabsTrigger value="system">System</TabsTrigger>
-        </TabsList>
-        <TabsContent value="all">
-          <NotificationsList filter="all" />
-        </TabsContent>
-        <TabsContent value="orders">
-          <NotificationsList filter="order" />
-        </TabsContent>
-        <TabsContent value="promotions">
-          <NotificationsList filter="promotion" />
-        </TabsContent>
-        <TabsContent value="system">
-          <NotificationsList filter="system" />
-        </TabsContent>
-      </Tabs>
+      <div className="px-4">
+        <Tabs defaultValue="all" className="mt-6">
+          <TabsList className="grid w-full grid-cols-4 md:w-auto">
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="orders">Orders</TabsTrigger>
+            <TabsTrigger value="promotions">Promotions</TabsTrigger>
+            <TabsTrigger value="system">System</TabsTrigger>
+          </TabsList>
+          <TabsContent value="all">
+            <NotificationsList filter="all" />
+          </TabsContent>
+          <TabsContent value="orders">
+            <NotificationsList filter="order" />
+          </TabsContent>
+          <TabsContent value="promotions">
+            <NotificationsList filter="promotion" />
+          </TabsContent>
+          <TabsContent value="system">
+            <NotificationsList filter="system" />
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
