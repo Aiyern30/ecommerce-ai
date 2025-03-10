377d7895b3e9ceca595e5945f0a643bcd045c621
Aiyern30 <ianbian2@gmail.com>
2025-03-10 10:28:37 +0800

    create notification page with tablist to show different components

---
+++ app/Notifications/page.tsx
@@ -0,0 +1,32 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui";
+import { NotificationsHeader } from "./NotificationHeader";
+import { NotificationsList } from "./NotificationList";
+
+export default function NotificationsPage() {
+  return (
+    <div className="container mx-auto py-8 px-4 md:px-6">
+      <NotificationsHeader />
+
+      <Tabs defaultValue="all" className="mt-6">
+        <TabsList className="grid w-full grid-cols-4 md:w-auto">
+          <TabsTrigger value="all">All</TabsTrigger>
+          <TabsTrigger value="orders">Orders</TabsTrigger>
+          <TabsTrigger value="promotions">Promotions</TabsTrigger>
+          <TabsTrigger value="system">System</TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          <NotificationsList filter="all" />
+        </TabsContent>
+        <TabsContent value="orders">
+          <NotificationsList filter="order" />
+        </TabsContent>
+        <TabsContent value="promotions">
+          <NotificationsList filter="promotion" />
+        </TabsContent>
+        <TabsContent value="system">
+          <NotificationsList filter="system" />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
