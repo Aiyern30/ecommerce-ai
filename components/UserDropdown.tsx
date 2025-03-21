303bc82f2fb1249a7d8a5311f8cfcb51ce6374d4
Aiyern30 <ianbian2@gmail.com>
2025-03-21 09:24:55 +0800

    `Refactor UserDropdown component to use DropdownMenu from ui components`

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,43 +1,47 @@
-import { useState } from "react";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/";
 import { Session } from "next-auth";
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/";
 
 interface UserDropdownProps {
   session: Session;
 }
 
 export default function UserDropdown({ session }: UserDropdownProps) {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <div className="relative">
-      <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none">
         <Avatar className="h-9 w-9 border-2 border-[#ff7a5c]">
           <AvatarImage src={session.user?.image || undefined} />
           <AvatarFallback>
             {session.user?.name ? session.user.name.charAt(0) : "U"}
           </AvatarFallback>
         </Avatar>
-      </button>
-
-      {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md border p-2">
-          <Link
-            href="/profile"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-          >
-            Profile
-          </Link>
-          <button
-            onClick={() => signOut()}
-            className="w-full text-left px-4 py-2 text-sm text-red-500 hover:bg-gray-100"
-          >
-            Logout
-          </button>
-        </div>
-      )}
-    </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-48">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/profile">Profile</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => signOut()}
+          className="text-red-500 cursor-pointer"
+        >
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
