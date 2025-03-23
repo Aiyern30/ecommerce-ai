ea4ddcb45a17b7a743115751684e85e674da3963
Aiyern30 <ianbian2@gmail.com>
2025-03-23 11:32:45 +0800

    `Refactor Layout component to use StaffLayout for staff routes`

--- components/StaffLayout.tsx
+++ components/StaffLayout.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import type React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import { Toaster } from "./ui";
+import type { ReactNode } from "react";
+import { Toaster } from "@/components/ui";
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "./ThemeProvider";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import DashboardLayout from "./Dashboard/DashboardLayout";
 
 interface LayoutProps {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
-export function CustomerLayout({ children }: LayoutProps) {
+export default function StaffLayout({ children }: LayoutProps) {
   return (
     <ThemeProvider
       attribute="class"
@@ -20,15 +19,8 @@ export function CustomerLayout({ children }: LayoutProps) {
       disableTransitionOnChange
     >
       <SessionProvider>
-        <div className="flex min-h-screen flex-col">
-          <Header />
-          <main className="flex-1 pt-[70px]">
-            {children}
-            <Toaster />
-          </main>
-
-          <Footer />
-        </div>
+        <DashboardLayout>{children}</DashboardLayout>
+        <Toaster />
       </SessionProvider>
     </ThemeProvider>
   );
