da518dd43d1f563f3971f351731cacf0f72f4399
Aiyern30 <ianbian2@gmail.com>
2025-03-23 11:32:45 +0800

    `Refactor Layout component to use StaffLayout for staff routes`

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,16 @@
 
 import { usePathname } from "next/navigation";
 import { CustomerLayout } from "@/components/CustomerLayout";
+import StaffLayout from "./StaffLayout";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isStaffRoute = pathname.startsWith("/Staff");
 
   return isStaffRoute ? (
-    <>{children}</>
+    <>
+      <StaffLayout>{children}</StaffLayout>
+    </>
   ) : (
     <CustomerLayout>{children}</CustomerLayout>
   );
