fe3824cdf4c173670bb7bcf224dd29f8a54ed712
Aiyern30 <ianbian2@gmail.com>
2025-03-23 17:27:42 +0800

    `Update margin in OrdersOverTimeChart component`

--- components/Dashboard/OrdersOverTime.tsx
+++ components/Dashboard/OrdersOverTime.tsx
@@ -83,7 +83,7 @@ export function OrdersOverTimeChart() {
             <ResponsiveContainer width="100%" height="100%">
               <AreaChart
                 data={data}
-                margin={{ top: 10, right: 10, left: 0, bottom: 0 }}
+                margin={{ top: 10, right: 10, left: -40, bottom: 0 }}
               >
                 <defs>
                   <linearGradient id="colorMay22" x1="0" y1="0" x2="0" y2="1">
