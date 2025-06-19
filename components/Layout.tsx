d64488da7344a92e172d66b4c9d61e15883e49c4
Aiyern30 <ianbian2@gmail.com>
2025-06-19 20:51:01 +0800

    `Refactor layout and add SupabaseProvider`

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,25 +1,15 @@
 "use client";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { usePathname } from "next/navigation";
 import { CustomerLayout } from "@/components/CustomerLayout";
 import StaffLayout from "./StaffLayout";
-import { useState } from "react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isStaffRoute = pathname.startsWith("/Staff");
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
   return isStaffRoute ? (
-    <>
-      <SessionContextProvider supabaseClient={supabaseClient}>
-        <StaffLayout>{children}</StaffLayout>
-      </SessionContextProvider>
-    </>
+    <StaffLayout>{children}</StaffLayout>
   ) : (
-    <SessionContextProvider supabaseClient={supabaseClient}>
-      <CustomerLayout>{children}</CustomerLayout>
-    </SessionContextProvider>
+    <CustomerLayout>{children}</CustomerLayout>
   );
 }
