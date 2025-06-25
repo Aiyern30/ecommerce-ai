939b3bc9c5130a39bbb8995b589f41d5ec50a3a0
Aiyern30 <ianbian2@gmail.com>
2025-06-25 21:46:30 +0800

    `Removed unused code and deleted unnecessary files`

--- app/(Customer)/layout.tsx
+++ app/(Customer)/layout.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useSession } from "@supabase/auth-helpers-react";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { CustomerLayout } from "@/components/CustomerLayout";
 
 export default function CustomerLayoutWrapper({
@@ -10,15 +7,5 @@ export default function CustomerLayoutWrapper({
 }: {
   children: React.ReactNode;
 }) {
-  const session = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (session === undefined) return;
-    if (!session?.user) {
-      router.push("/login");
-    }
-  }, [router, session]);
-
   return <CustomerLayout>{children}</CustomerLayout>;
 }
