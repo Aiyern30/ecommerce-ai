77af9ff169ce9a57ff4e9cdc332c5822e232b6dd
Aiyern30 <ianbian2@gmail.com>
2025-06-25 22:00:56 +0800

    `Removed authentication and routing logic from StaffLayoutWrapper component`

--- app/(Staff)/layout.tsx
+++ app/(Staff)/layout.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useSession } from "@supabase/auth-helpers-react";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import StaffLayout from "@/components/StaffLayout";
 
 export default function StaffLayoutWrapper({
@@ -10,19 +7,5 @@ export default function StaffLayoutWrapper({
 }: {
   children: React.ReactNode;
 }) {
-  const session = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (session === undefined) return;
-    const role = session?.user?.user_metadata?.role;
-
-    if (!session?.user) {
-      router.push("/login");
-    } else if (role !== "staff") {
-      router.push("/unauthorized");
-    }
-  }, [router, session]);
-
   return <StaffLayout>{children}</StaffLayout>;
 }
