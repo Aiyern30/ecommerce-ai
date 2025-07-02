e2eeb40a12cf219b1c4ad90d481801cc4af8cdff
Aiyern30 <ianbian2@gmail.com>
2025-07-02 17:59:26 +0800

    created blogs type

---
+++ type/blogs.ts
@@ -0,0 +1,24 @@
+export interface Blog {
+  id: string;
+  title: string;
+  description: string | null;
+  external_link: string | null;
+  created_at: string;
+  updated_at: string;
+  blog_images?: { image_url: string }[] | null;
+  blog_tags?:
+    | {
+        tags: {
+          id: string;
+          name: string;
+        };
+      }[]
+    | null;
+}
+
+export interface Tag {
+  id: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
