35195437592af647f3e4285f7e2a5a90b6b4fefc
Aiyern30 <ianbian2@gmail.com>
2025-07-08 20:25:30 +0800

    created permission redirect to 401 page

--- components/StaffLayout.tsx
+++ components/StaffLayout.tsx
@@ -1,16 +1,43 @@
 "use client";
 
-import type { ReactNode } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/supabase/browserClient";
 import { Toaster } from "@/components/ui";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import DashboardLayout from "./Dashboard/DashboardLayout";
+import type { ReactNode } from "react";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function StaffLayout({ children }: LayoutProps) {
+  const router = useRouter();
+  const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.getUser();
+
+      if (error || !user || user.user_metadata.role !== "staff") {
+        router.replace("/401");
+      } else {
+        setIsAuthorized(true);
+      }
+    };
+
+    checkUser();
+  }, [router]);
+
+  if (isAuthorized === null) {
+    return null; // Or loading spinner
+  }
+
   return (
     <ThemeProvider
       attribute="class"
