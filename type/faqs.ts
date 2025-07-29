90fd7576f96108d99f7c595221a081a570b51c63
Aiyern30 <ianbian2@gmail.com>
2025-07-29 16:31:44 +0800

    `Update Faq type to include section_id and section properties`

--- type/faqs.ts
+++ type/faqs.ts
@@ -2,7 +2,11 @@ export type Faq = {
   id: string;
   question: string;
   answer: string;
-  section: string;
+  section_id: string | null;
+  section?: {
+    name: string;
+  } | null;
+  status: "draft" | "published" | "archived";
   created_at: string;
   updated_at: string;
 };
