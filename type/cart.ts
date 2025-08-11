e35b678b6ccd67204a416b2d2cd53f4f5a39d41a
Aiyern30 <ianbian2@gmail.com>
2025-08-11 17:07:40 +0800

    `feat: Add stock quantity checks and updates to cart functionality`

--- type/cart.ts
+++ type/cart.ts
@@ -19,6 +19,7 @@ export interface CartItem {
     tremie_3_price: number | null;
     unit: string | null;
     image_url: string;
+    stock_quantity: number | null;
     product_images?:
       | {
           id: string;
