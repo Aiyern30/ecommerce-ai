ee60e03093a19220796ceea21e2c1ee0f15eb9e1
Aiyern30 <ianbian2@gmail.com>
2025-08-21 19:30:32 +0800

    `Refactor CartPage and Footer components with updated skeleton and layout designs`

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,9 +12,9 @@ const Footer = () => {
                 YTL CONCRETE HUB
               </TypographyH3>
               <TypographyP className="text-sm text-muted-foreground dark:text-gray-400">
-                YTL Concrete Hub is your trusted source for high-quality concrete
-                and mortar products, serving Malaysian construction projects with
-                reliable materials and expert support.
+                YTL Concrete Hub is your trusted source for high-quality
+                concrete and mortar products, serving Malaysian construction
+                projects with reliable materials and expert support.
               </TypographyP>
             </div>
             <div>
