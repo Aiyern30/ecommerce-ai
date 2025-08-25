0860f94ff1cbe8c0009ebf6250d9698198e51e12
Aiyern30 <ianbian2@gmail.com>
2025-08-26 00:52:08 +0800

    `Refactor StatsCards component to use Typography components and update currency formatting utility`

--- lib/utils/currency.ts
+++ lib/utils/currency.ts
@@ -1,4 +1,10 @@
 export const formatCurrency = (amount: number) => {
+  if (amount >= 1_000_000) {
+    return `RM ${(amount / 1_000_000).toFixed(2)}m`;
+  }
+  if (amount >= 1_000) {
+    return `RM ${(amount / 1_000).toFixed(2)}k`;
+  }
   return new Intl.NumberFormat("en-MY", {
     style: "currency",
     currency: "MYR",
