20ad363b4cf38819dbf45fd5c38e282b86ab5962
Aiyern30 <ianbian2@gmail.com>
2025-12-17 13:26:58 +0800

    `Added unoptimized option to images configuration in next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     domains: [
       "hebbkx1anhila5yf.public.blob.vercel-storage.com",
       "lh3.googleusercontent.com",
